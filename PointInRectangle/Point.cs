daa2c8d54aea3cf02dd05a42de4b4d98e960f460
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-29 11:12:20 +0300

    Add new

---
+++ PointInRectangle/Point.cs
@@ -0,0 +1,15 @@
+﻿namespace PointInRectangle
+{
+    public class Point
+    {
+
+        public Point(double coordinateX, double coordinateY)
+        {
+            this.CoordinateX = coordinateX;
+            this.CoordinateY = coordinateY;
+        }
+        public double CoordinateX { get; set; }
+
+        public double CoordinateY { get; set; }
+    }
+}
