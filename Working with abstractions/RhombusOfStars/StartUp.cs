7615218c61002d3f26ad84915aebc41b33619246
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-29 13:57:00 +0300

    Add new

---
+++ Working with abstractions/RhombusOfStars/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RhombusOfStars
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int stars = int.Parse(Console.ReadLine());
+
+            for (int starCount = 1; starCount <= stars; starCount++)
+            {
+                PrintRow(stars, starCount);
+            }
+
+            for (int starCount = stars-1 ; starCount >= 1; starCount--)
+            {
+                PrintRow(stars, starCount);
+            }
+        }
+
+        private static void PrintRow(int stars, int starCount)
+        {
+            int spaces = stars - starCount;
+
+            Console.Write(new string(' ', spaces));
+
+            for (int i = 1; i <= starCount; i++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+
+        }
+
+    }
+}
