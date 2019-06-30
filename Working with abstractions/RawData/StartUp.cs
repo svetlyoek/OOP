45467d02ca2430df301eb0d336a556ae3f0cbefe
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-30 14:00:41 +0300

    Add new

---
+++ Working with abstractions/RawData/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RawData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string[] carInfo = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string model = carInfo[0];
+                int engineSpeed = int.Parse(carInfo[1]);
+                int enginePower = int.Parse(carInfo[2]);
+                int cargoWeight = int.Parse(carInfo[3]);
+                string cargoType = carInfo[4];
+                double tire1Pressure = double.Parse(carInfo[5]);
+                double tire2Pressure = double.Parse(carInfo[7]);
+                double tire3Pressure = double.Parse(carInfo[9]);
+                double tire4Pressure = double.Parse(carInfo[11]);
+
+                var engine = new Engine(engineSpeed, enginePower);
+                var cargo = new Cargo(cargoWeight, cargoType);
+                var currentTires = new Tire[4]
+                {
+                     new Tire(tire1Pressure,int.Parse(carInfo[6])),
+                     new Tire(tire2Pressure,int.Parse(carInfo[8])),
+                     new Tire(tire3Pressure,int.Parse(carInfo[10])),
+                     new Tire(tire4Pressure,int.Parse(carInfo[12])),
+
+                };
+
+                var currentCar = new Car(model, engine, cargo, currentTires);
+                cars.Add(currentCar);
+
+
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "fragile")
+            {
+                List<string> fragile = cars
+                    .Where(x => x.Cargo.Type == "fragile" && x.Tires.Any(y => y.Pressure < 1))
+                    .Select(x => x.Model)
+                    .ToList();
+
+                Console.WriteLine(string.Join(Environment.NewLine, fragile));
+            }
+            else if (command == "flamable")
+            {
+                List<string> flamable = cars
+                    .Where(x => x.Cargo.Type == "flamable" && x.Engine.Power > 250)
+                    .Select(x => x.Model)
+                    .ToList();
+
+                Console.WriteLine(string.Join(Environment.NewLine, flamable));
+            }
+        }
+    }
+}
