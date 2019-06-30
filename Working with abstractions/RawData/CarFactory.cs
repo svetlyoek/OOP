48842edae1a1b1cf5c591227be8e72568a9abcab
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-30 20:26:27 +0300

    Add new

---
+++ Working with abstractions/RawData/CarFactory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace RawData
+{
+    public class CarFactory
+    {
+        public CarFactory()
+        {
+            this.Cars = new List<Car>();
+        }
+
+        public List<Car> Cars { get; private set; }
+
+        public void Add(string[] carInfo)
+        {
+
+            string model = carInfo[0];
+            int engineSpeed = int.Parse(carInfo[1]);
+            int enginePower = int.Parse(carInfo[2]);
+            int cargoWeight = int.Parse(carInfo[3]);
+            string cargoType = carInfo[4];
+            double tire1Pressure = double.Parse(carInfo[5]);
+            double tire2Pressure = double.Parse(carInfo[7]);
+            double tire3Pressure = double.Parse(carInfo[9]);
+            double tire4Pressure = double.Parse(carInfo[11]);
+
+            var engine = new Engine(engineSpeed, enginePower);
+            var cargo = new Cargo(cargoWeight, cargoType);
+            var currentTires = new Tire[4]
+            {
+                     new Tire(tire1Pressure,int.Parse(carInfo[6])),
+                     new Tire(tire2Pressure,int.Parse(carInfo[8])),
+                     new Tire(tire3Pressure,int.Parse(carInfo[10])),
+                     new Tire(tire4Pressure,int.Parse(carInfo[12])),
+
+            };
+
+            var currentCar = new Car(model, engine, cargo, currentTires);
+            this.Cars.Add(currentCar);
+
+        }
+
+        public List<Car> GetCar()
+        {
+            return this.Cars;
+        }
+
+
+
+    }
+}
---
+++ Working with abstractions/RawData/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RawData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var carFactory = new CarFactory();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] carInfo = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                carFactory.Add(carInfo);
+
+            }
+
+            string command = Console.ReadLine();
+
+            PrintOutput(command, carFactory);
+        }
+
+        private static void PrintOutput(string command, CarFactory carFactory)
+        {
+            if (command == "fragile")
+            {
+                List<string> fragile = carFactory.GetCar()
+                    .Where(x => x.Cargo.Type == "fragile" && x.Tires.Any(y => y.Pressure < 1))
+                    .Select(x => x.Model)
+                    .ToList();
+
+                Console.WriteLine(string.Join(Environment.NewLine, fragile));
+            }
+            else if (command == "flamable")
+            {
+                List<string> flamable = carFactory.GetCar()
+                    .Where(x => x.Cargo.Type == "flamable" && x.Engine.Power > 250)
+                    .Select(x => x.Model)
+                    .ToList();
+
+                Console.WriteLine(string.Join(Environment.NewLine, flamable));
+            }
+
+        }
+    }
+}
