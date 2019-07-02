24b3965b90b190a7e3719a38002c344407e6b0ef
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-07-02 18:28:16 +0300

    Add new

---
+++ Working with abstractions/HospitalWithClasses/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace HospitalWithClasses
+{
+    public class Patient
+    {
+        public Patient(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Working with abstractions/HospitalWithClasses/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace HospitalWithClasses
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var hospital = new Hospital();
+            var engine = new Engine(hospital);
+
+            engine.Run();
+        }
+    }
+}
