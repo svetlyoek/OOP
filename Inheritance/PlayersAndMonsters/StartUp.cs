a3f5daa67d9dfb96300517979b727aeaeb968428
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-07-03 18:27:56 +0300

    Add new

---
+++ Inheritance/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string heroName = Console.ReadLine();
+            int heroLevel = int.Parse(Console.ReadLine());
+
+            var elf = new Elf(heroName, heroLevel);
+            var knight = new Knight(heroName, heroLevel);
+
+            Console.WriteLine(elf);
+            Console.WriteLine(knight);
+
+
+        }
+    }
+}
---
+++ Inheritance/Zoo/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string gorillaName = Console.ReadLine();
+            string snakeName = Console.ReadLine();
+
+            var gorilla = new Gorilla(gorillaName);
+            var snake = new Snake(snakeName);
+
+            Console.WriteLine($"Gorilla's name: {gorilla.Name}");
+            Console.WriteLine($"Snake's name: {snake.Name}");
+
+        }
+    }
+}
