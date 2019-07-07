84d1cf29a605a731f21baa99000faf52d75ceae8
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-07-07 13:32:11 +0300

    Add new

---
+++ Encapsulation/ShoppingSpree/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ShoppingSpree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Program
+    {
+        public static void Main()
+        {
+            var engine = new Engine();
+            engine.Run();
+        }
+
+    }
+}
