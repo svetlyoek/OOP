27d650f9439b9633896365ef57d7d250f1161a84
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-07-10 14:50:15 +0300

    Add new

---
+++ Interfaces and abstraction/Telephony/smartphone.cs
@@ -0,0 +1,71 @@
+﻿namespace Telephony
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Smartphone : ICall, IBrowse
+    {
+        private string number;
+        private string url;
+
+        public string Number
+        {
+            get
+            {
+                return this.number;
+            }
+            set
+            {
+                if (CheckNumber(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.invalidNumber);
+                }
+
+                this.number = value;
+            }
+        }
+
+
+        public string Url
+        {
+            get
+            {
+                return this.url;
+            }
+
+            set
+            {
+                if (CheckUrl(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.invalidUrl);
+                }
+
+                this.url = value;
+            }
+
+        }
+
+        private bool CheckUrl(string value)
+        {
+            return value.FirstOrDefault(u => Char.IsDigit(u)) != 0;
+        }
+
+        private bool CheckNumber(string value)
+        {
+            return value.FirstOrDefault(n => !Char.IsDigit(n)) != 0;
+        }
+
+
+        public string CallNumber()
+        {
+            return $"Calling... {this.Number}";
+        }
+
+        public string PrintUrl()
+        {
+            return $"Browsing: {this.Url}!";
+        }
+    }
+}
