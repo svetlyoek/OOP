8ab8630e4bc324f64832b30037c3845c73a9defc
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-07-21 14:19:17 +0300

    Add new

---
+++ Reflection and atributes/CodeTracker/AuthorAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+public class AuthorAttribute : Attribute
+{
+    public AuthorAttribute(string name)
+    {
+        this.Name = name;
+    }
+    public string Name { get; set; }
+}
+
---
+++ Reflection and atributes/CodeTracker/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        var type = typeof(StartUp);
+        var methods = type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var method in methods)
+        {
+            if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+            {
+                var attributes = method.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute attribute in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                }
+            }
+        }
+    }
+}
+
